dc1c6e7894e7ebc995df32cbfb2668b400a8cdd5
Wesley Thauan <thauanblox@gmail.com>
2024-12-20 08:06:23 -0300

    Mysql3.sql

--- Mysql3.sql
+++ Mysql3.sql
@@ -12,3 +12,37 @@ CREATE TABLE Fornecedores (
     NomeFornecedor VARCHAR(100) NOT NULL,
     Cidade VARCHAR(100)
 );
+
+
+CREATE TABLE Estoque (
+    EstoqueID INT AUTO_INCREMENT PRIMARY KEY,
+    ProdutoID INT,
+    FornecedorID INT,
+    Quantidade INT NOT NULL,
+    DataEntrada DATE NOT NULL,
+    FOREIGN KEY (ProdutoID) REFERENCES Produtos(ProdutoID),
+    FOREIGN KEY (FornecedorID) REFERENCES Fornecedores(FornecedorID)
+);
+
+SELECT *
+FROM Estoque e
+LEFT JOIN Produtos p ON e.ProdutoID = p.ProdutoID
+LEFT JOIN Fornecedores f ON e.FornecedorID = f.FornecedorID
+
+UNION
+
+SELECT *
+FROM Estoque e
+RIGHT JOIN Produtos p ON e.ProdutoID = p.ProdutoID
+RIGHT JOIN Fornecedores f ON e.FornecedorID = f.FornecedorID;
+
+SELECT ProdutoID, SUM(Quantidade) AS QuantidadeTotal
+FROM Estoque
+GROUP BY ProdutoID;
+
+SELECT ProdutoID, SUM(Quantidade) AS QuantidadeTotal
+FROM Estoque
+GROUP BY ProdutoID;
+
+ALTER TABLE Estoque
+ADD COLUMN LocalArmazenamento VARCHAR(100);
